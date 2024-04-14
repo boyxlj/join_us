14ca5b625331e1311bbc3fbb6b81cede1008ba56
boyxlj <x709500@163.com>
2024-04-14 14:03:14 +0800

    feat:修复已知问题

--- join_us_manger/src/api/upload.ts
+++ join_us_manger/src/api/upload.ts
@@ -1,3 +1,3 @@
 //上传用户头像
-export const uploadUserAvatarUrl = import.meta.env.VITE_APP_BASE_URL+`/manger/avatar`
-export const uploadPhotoUrl = import.meta.env.VITE_APP_BASE_URL+`/manger/photo`
+export const uploadUserAvatarUrl = import.meta.env.VITE_APP_BASE_URL + `/avatar`
+export const uploadPhotoUrl = import.meta.env.VITE_APP_BASE_URL + `/photo`
